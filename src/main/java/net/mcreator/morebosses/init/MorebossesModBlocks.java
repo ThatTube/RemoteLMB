ded1e18c6719c86aed75f348dbaae4923e99956b
ThatTube <164914586+LucaTube@users.noreply.github.com>
2026-01-04 16:14:24 -0300

    First Commit

---
+++ src/main/java/net/mcreator/morebosses/init/MorebossesModBlocks.java
@@ -0,0 +1,21 @@
+
+/*
+ *    MCreator note: This file will be REGENERATED on each build.
+ */
+package net.mcreator.morebosses.init;
+
+import net.minecraftforge.registries.RegistryObject;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.DeferredRegister;
+
+import net.minecraft.world.level.block.Block;
+
+import net.mcreator.morebosses.block.JALAMBIBlock;
+import net.mcreator.morebosses.MorebossesMod;
+
+public class MorebossesModBlocks {
+	public static final DeferredRegister<Block> REGISTRY = DeferredRegister.create(ForgeRegistries.BLOCKS, MorebossesMod.MODID);
+	public static final RegistryObject<Block> JALAMBI = REGISTRY.register("jalambi", () -> new JALAMBIBlock());
+	// Start of user code block custom blocks
+	// End of user code block custom blocks
+}
