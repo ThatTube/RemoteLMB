ea42bddb833df247d56dd88ed174c6c114d0b65f
ThatTube <164914586+LucaTube@users.noreply.github.com>
2026-01-05 19:22:35 -0300

    Comit Agora o fluidos de amgma dão 3 de dano na colisão

--- src/main/java/net/mcreator/morebosses/block/RawMagmaBlock.java
+++ src/main/java/net/mcreator/morebosses/block/RawMagmaBlock.java
@@ -5,15 +5,26 @@ import org.checkerframework.checker.units.qual.s;
 
 import net.minecraft.world.level.material.PushReaction;
 import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.LiquidBlock;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.core.BlockPos;
 
+import net.mcreator.morebosses.procedures.GreenMagmaMobplayerCollidesBlockProcedure;
 import net.mcreator.morebosses.init.MorebossesModFluids;
 
 public class RawMagmaBlock extends LiquidBlock {
 	public RawMagmaBlock() {
 		super(() -> MorebossesModFluids.RAW_MAGMA.get(), BlockBehaviour.Properties.of().mapColor(MapColor.FIRE).strength(100f).hasPostProcess((bs, br, bp) -> true).emissiveRendering((bs, br, bp) -> true).lightLevel(s -> 15).noCollission()
 				.noLootTable().liquid().pushReaction(PushReaction.DESTROY).sound(SoundType.EMPTY).replaceable());
+	}
+
+	@Override
+	public void entityInside(BlockState blockstate, Level world, BlockPos pos, Entity entity) {
+		super.entityInside(blockstate, world, pos, entity);
+		GreenMagmaMobplayerCollidesBlockProcedure.execute(world, entity);
 	}
 }
